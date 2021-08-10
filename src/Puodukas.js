b08ff43b2bc82d42ec03ebf411174bee19fe20b8
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-08-10 22:06:07 +0300

    ProjSenas

---
+++ src/Puodukas.js
@@ -0,0 +1,14 @@
+class Puodukas {
+
+    constructor () {
+        this.spalva = "baltas";
+        this.turis = 150;
+        this.kiekis = 0;
+
+    }
+}
+
+module.exports = {
+    Puodukas
+};
+    
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const {Puodukas} = require ("./Puodukas.js");
+
+console.log(Puodukas);
+
+console.log("Hello World !!!");
+
+let p = new Puodukas();
+
+console.log(p);
